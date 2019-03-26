fa9aa202e1ee9b5d5fc6a34c17d3d289102e0226
Radik Fattakhov <radikft@gmail.com>
2019-03-26 12:03:17 +0300

    Add condition in case of linux

---
+++ pod-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    cd ./ios && pod install
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo 'WARNING: skipping pod install on Linux'
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo 'WARNING: skipping pod install on Windows x32'
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    echo 'WARNING: skipping pod install on Windows x64'
+fi
