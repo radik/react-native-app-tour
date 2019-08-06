1d9913e80eaa03240973f715c1ab927c441d11f8
Radik Fattakhov <radikft@gmail.com>
2019-08-06 23:03:30 +0300

    Return import syntax back

--- ios/RNAppTour.h
+++ ios/RNAppTour.h
@@ -1,6 +1,6 @@
 
 #import <React/RCTUIManager.h>
-#import "MaterialShowcase/MaterialShowcase-Swift.h"
+#import <MaterialShowcase/MaterialShowcase-Swift.h>
 
 @interface MutableOrderedDictionary<__covariant KeyType, __covariant ObjectType> : NSDictionary<KeyType, ObjectType>
 @end
